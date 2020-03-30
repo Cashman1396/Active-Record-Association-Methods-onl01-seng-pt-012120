87df98191fed074e02ae953e1e0e06c046e49ff0
Chris Ashman <chrisashman96@gmail.com>
2020-03-30 16:12:15 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self. genres.first
+    self.song.first.genre
   end
 
   def song_count
-    
+    self.songs.length
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+  self.genres.length
   end
 end
